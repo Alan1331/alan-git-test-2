ab52ebb955e1a638e9b32694eeec32aa3d5bc4b8
Alan133 <sahlan.royale@gmail.com>
2022-03-02 20:29:12 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae nemo repudiandae optio dolore explicabo possimus quibusdam placeat excepturi consectetur incidunt, sint delectus error saepe ducimus, esse voluptate. Quas, temporibus impedit.</p>
 </body>
 </html>
